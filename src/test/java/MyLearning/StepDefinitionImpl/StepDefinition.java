31adc02fd634972f8bcf600c862538e52e50cc4d
venkatesh17051999 <venkateshesakkin@gmail.com>
2025-01-04 18:36:47 +0530

    Update StepDefinition.java

--- src/test/java/MyLearning/StepDefinitionImpl/StepDefinition.java
+++ src/test/java/MyLearning/StepDefinitionImpl/StepDefinition.java
@@ -2,9 +2,9 @@ package MyLearning.StepDefinitionImpl;
 
 public class StepDefinition extends BaseTestComponents{
 	
-	@Given ("I landed on Ecommerce Page")
+	
 	public class I_landed_on_Ecommerce_Page() {
-		launchPage();
+		
 	}
 	
 	
